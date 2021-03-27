2f85bec9c9f061f1fec68450e90694d44fe8a250
Karandeep Singh <karandeepjabbal@gmail.com>
2021-03-27 19:42:36 +0530

    sql conn and post routes

--- dbcon.js
+++ dbcon.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db =  mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 1234,
-    database: 'trace-x'
+    password: '',
+    database: 'trace'
 });
 
 db.connect((err) => {
--- index.js
+++ index.js
@@ -26,6 +26,38 @@ app.post('/get', (req, res) => {
     res.send('Recieved');
 })
 
+app.post('/signup', (req,res) => {
+    console.log(req.body);
+    data = {
+        name : req.body.name,
+        username : req.body.username,
+        email : req.body.email,
+        password : req.body.password
+    };
+    db.query('INSERT INTO user SET ?',data,(err,res)=>{
+        if(err) throw err;
+        console.log(res);
+        
+    });   
+    res.send("Successful Sign up");
+}); 
+app.post('/login',(req,res) => {
+     
+    let sql= 'SELECT * FROM user WHERE username = ?';
+    //console.log(data);
+    db.query(sql,req.body.username,(err,resu) => {
+        if(err) throw err; 
+        
+        console.log(resu); 
+        if(resu.length<1)
+        res.send("Invalid username or password");
+        else if(resu[0].password == req.body.password) 
+        res.send("Successful Login"); 
+        else res.send("Invalid username or password");
+    });   
+    
+});
+
 app.listen(port,()=>{
     console.log(`Running on port ${port}`);
 });
